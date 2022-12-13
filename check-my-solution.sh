5cbe9ea1ca747f13f086080d08762da8e6aeddf2
pawelktk <pawel.klopotek@gmail.com>
2022-12-13 23:59:17 +0100

    Working prototype

--- check-my-solution.sh
+++ check-my-solution.sh
@@ -2,7 +2,7 @@
 
 executable="$1"
 tests="$executable.test"
-time_limit=2
+time_limit=10
 
 if ! [[ -f "$executable" ]]; then
 	echo "Error: $executable doesn't exist"
@@ -14,9 +14,10 @@ if ! [[ -f "$tests" ]]; then
 fi
 
 number_of_lines=$(wc -l "$tests" | awk '{print $1}')
+((number_of_lines++))
 mode="test"
 is_first=1
-test_regex=" "
+
 for ((i = 1; i <= number_of_lines + 1; i++)); do
 	if ((i != number_of_lines + 1)); then
 		current_line="$(awk "NR==$i" "$tests")"
@@ -34,20 +35,38 @@ for ((i = 1; i <= number_of_lines + 1; i++)); do
 			input="$(echo "$input" | tr '\n' ' ' | tr -s '[:blank:]' | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
 			output="$(echo "$output" | tr '\n' ' ' | tr -s '[:blank:]' | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
 			finished_gracefuly=0
-			actual_output="$(timeout $time_limit echo "$input" | "$executable" | tr '\n' ' ' | tr -s '[:blank:]' | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' && finished_gracefuly=1)"
-			execution_time="$( TIMEFORMAT='%lR';time ( sh -c "echo \"$input\" | \"$executable\"" ) 2>&1 1>/dev/null)"
-			
+			if actual_output="$(timeout $time_limit echo "$input" | "$executable" | tr '\n' ' ' | tr -s '[:blank:]' | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"; then
+				finished_gracefuly=1
+			fi
 			echo "[TEST $test_n]"
-			echo "Input:"
-			echo "$input"
-			echo "Output:"
-			echo "$actual_output"
-			echo "Expected output:"
-			echo "$output"
-			echo "Time:"
-			echo "$execution_time"
+			if ((finished_gracefuly == 0)); then
+				echo "ERROR: Execution time exceeded the time limit ($time_limit)"
+			elif [[ "$actual_output" != "$output" ]]; then
+				execution_time="$(
+					TIMEFORMAT='%lR'
+					time (sh -c "echo \"$input\" | \"$executable\"") 2>&1 1>/dev/null
+				)"
+				echo "ERROR: Wrong output"
+				echo "Input:"
+				echo "$input"
+				echo "Expected output:"
+				echo "$output"
+				echo "Output:"
+				echo "$actual_output"
+				echo "Time:"
+				echo "$execution_time"
+			else
+				execution_time="$(
+					TIMEFORMAT='%lR'
+					time (sh -c "echo \"$input\" | \"$executable\"") 2>&1 1>/dev/null
+				)"
+				echo "OK"
+				echo "Time:"
+				echo "$execution_time"
+			fi
+
 			echo ""
-			
+
 			output=""
 			input=""
 		fi
@@ -62,7 +81,8 @@ for ((i = 1; i <= number_of_lines + 1; i++)); do
 	elif [[ "$mode" == "out" ]]; then
 		output+=" $current_line "
 	elif [[ "$mode" == "test" ]]; then
-		echo "test"
+		true
+		#echo "test"
 	fi
 
 done
